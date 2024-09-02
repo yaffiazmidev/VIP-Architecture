6273704191a6b7c7e75c509da314a99ead1d0556
Yaffi Azmi <44551686+yaffiazmidev@users.noreply.github.com>
2024-09-02 19:34:07 +0700

    initial commit

---
+++ VIP-Architecture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VIP-Architecture
+//
+//  Created by DENAZMI on 02/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ VIP-ArchitectureUITests/VIP_ArchitectureUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  VIP_ArchitectureUITestsLaunchTests.swift
+//  VIP-ArchitectureUITests
+//
+//  Created by DENAZMI on 02/09/24.
+//
+
+import XCTest
+
+final class VIP_ArchitectureUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
